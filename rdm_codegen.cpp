49df6aba58c887a7fa1d54764d3fd886d761e79a
Anshu-raj-co <59351492+Anshu-raj-co@users.noreply.github.com>
2022-10-22 00:20:29 +0530

    Update rdm_codegen.cpp

--- rdm_codegen.cpp
+++ rdm_codegen.cpp
@@ -8,3 +8,5 @@ main() {
    srand(time(0)); //set the upper bound to generate the random number
    cout << "The random number is: "<<rand()%maxi;
 }
+
+//this is a code to generate a random number in c++
